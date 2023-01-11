08dbfe2a72fc15750d1ff7379c97c3c82bd54abe
malk-carlos <81245539+malk-carlos@users.noreply.github.com>
2023-01-11 22:59:30 +0900

    ヘッダ更新
    ヘッダ共通化

---
+++ header.js
@@ -0,0 +1,9 @@
+function header(){
+    $.ajax({
+        url: "./header.html",
+        cache: false,
+        success: function(header){
+            $("header").html(header);
+        }
+    });
+}
