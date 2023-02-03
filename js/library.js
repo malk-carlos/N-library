7f962170bafaa413df3538863276d946bea26606
malk-carlos <81245539+malk-carlos@users.noreply.github.com>
2023-02-03 23:29:03 +0900

    Update library.js
    コード簡略化

--- js/library.js
+++ js/library.js
@@ -72,18 +72,14 @@ function mydata(datas) {
     const book_length = $(".book:last").attr("id").replace("book","");
     console.log(book_length,"Blen") 
     for(let i = 0; book_length > i; i ++){
+        if (Number($(`#btn${i+1}`).attr("name")) <= 0){
+            $(`#btn${i+1}`).removeClass('standby reserve_btn').addClass('limit').text("予約不可");
+        }
         for(let j = 0; datas.length > j; j ++){
             let dataJ = datas[j];
             console.log(dataJ[5],"dataJ5");
             if(dataJ[1] == $(`#book${i+1}`).attr("class").replace("book ","")){
-                $(`#btn${i+1}`).removeClass('standby').addClass('reserved').removeAttr("onClick").text("予約済み");
-            } else if (Number($(`#btn${i+1}`).attr("name")) <= 0){
-                $(`#btn${i+1}`).removeClass('standby reserve_btn').addClass('limit').text("予約不可");
-            }
-        }
-        if(datas.length <= 0){ // 予約履歴なしの場合の処理
-            if (Number($(`#btn${i+1}`).attr("name")) <= 0){
-                $(`#btn${i+1}`).removeClass('standby reserve_btn').addClass('limit').text("予約不可");
+                $(`#btn${i+1}`).removeClass('standby limit').addClass('reserved').removeAttr("onClick").text("予約済み");
             }
         }
     }
