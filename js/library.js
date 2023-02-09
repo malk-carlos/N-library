25f575e14b7dd8c7c96c3c155712c91009fa3d3c
malk-carlos <81245539+malk-carlos@users.noreply.github.com>
2023-02-09 22:45:25 +0900

    ポップアップ表示実装

--- js/library.js
+++ js/library.js
@@ -15,6 +15,7 @@ function test(bookDB) {
                 const $n = i*4+j;
                 const $btn = $(`<button id='btn${String($n)}' class='standby' name='${bookDB[$n][21]}' onClick='toReserve(${bookDB[$n][0]},${$n})'>予約不可</button>`) //予約ボタン
                 const $div = $(`<div id='book${String($n)}' class='book ${bookDB[$n][0]}'></div>`) // 各書籍の表紙, タイトル, 著者を記載する要素
+                const $div2 = $(`<div id='bookData${String($n)}' class='bookData' onClick='popup(${String($n)})'></div>`)
                 const $ps = $(`<div class='ps'><div>`)
     
                 const $cover = $(`<img src='${bookDB[$n][17]}' class='coverimg' alt="${bookDB[$n][1]}" oncontextmenu="return false;">`) // 表紙
@@ -22,7 +23,8 @@ function test(bookDB) {
                 const $writer = $(`<p class='writer'>${bookDB[$n][7]}</p>`) // 著者名
     
                 $ps.append($title).append($writer);
-                $div.append($cover).append($ps).append($btn);
+                $div2.append($cover).append($ps)
+                $div.append($div2).append($btn);
                 $row.append($div);
             }
             $("#container").append($row);
@@ -38,7 +40,7 @@ function toReserve(book_num,n) {
     $("#overlay").fadeIn(300);
     if ($(`#btn${String(n)}`).attr('class') == 'reserve_btn'){
         console.log("tore")
-        send("reserve",cheak().sub, book_num, new Date().toLocaleString(),n)
+        res_popup(book_num,n)
     }
 }
 
@@ -65,7 +67,7 @@ function limit () {
     if (userdata < 3) {
         $(`.standby`).removeClass('standby limit').addClass('reserve_btn').text("予約する");
     } else {
-        $(`.reserve_btn`).removeClass('reserve_btn').addClass('limit').text("予約不可");
+        $(`.reserve_btn`).removeClass('reserve_btn').addClass('limit').removeAttr("onClick").text("予約不可");
     }
 }
 
@@ -76,7 +78,7 @@ function mydata(logDB) {
     console.log(book_length,"Blen") 
     for(let i = 0; book_length > i; i ++){
         if (Number($(`#btn${i+1}`).attr("name")) <= 0){
-            $(`#btn${i+1}`).removeClass('standby reserve_btn').addClass('limit').text("予約不可");
+            $(`#btn${i+1}`).removeClass('standby reserve_btn').addClass('limit').removeAttr("onClick").text("予約不可");
         }
         for(let j = 0; logDB.length > j; j ++){
             let dataJ = logDB[j];
@@ -87,4 +89,46 @@ function mydata(logDB) {
         }
     }
     limit();
+}
+
+function popup(n) {
+    // 蔵書データ[n]でsweetalert表示
+    Swal.fire({
+        title: "書籍詳細",
+        html: `<p class='bookSwalName'><b>『${bookDB[n][1]}』</b><span class='ruby'>${bookDB[n][2]}</span><p>
+        <p class='bookSwalCoverP'><img src='${bookDB[n][17]}' class='bookSwalCover'></p>
+        <p class='bookSwalWriter'>著者：${bookDB[n][7]}</p>
+        <p class='bookSwalPage'>${bookDB[n][11]}ページ</p>
+        <p class='bookSwalData'><span class='bookSwalRegistry'>登録数：${bookDB[n][18]}冊</span>｜<span class='bookSwalStock'>貸出可能在庫：${bookDB[n][21]}冊</span></p>`,
+        backdrop: "#0005",
+        customClass: "bookDetails"
+    })
+}
+
+function res_popup(book_num,n) {
+    // 蔵書データ[n]でsweetalert表示
+    // 予約時最終確認
+    Swal.fire({
+        title: "最終確認",
+        html: `<p class='bookSwalName'>${bookDB[n][7]} の<br><b>『${bookDB[n][1]}』</b><br>を予約します。よろしいですか？</p>
+        <p class='bookSwalCoverP'><img src='${bookDB[n][17]}' class='bookSwalCover'>`,
+        showCancelButton : true,
+        cancelButtonText : 'やめる',
+        showLoaderOnConfirm: true,
+        customClass: "finalConfirmation"
+    }).then((result)=>{
+        console.log("then1")
+        if(result.isConfirmed){
+            send("reserve",cheak().sub, book_num, new Date().toLocaleString(),n)
+        } else {
+            console.log("then2")
+            Swal.fire({
+                title: "予約をキャンセルしました",
+                text: "",
+                customClass:"canceled"
+            }).then(()=>{
+                $("#overlay").fadeOut(300);
+            })
+        }
+    })
 }
